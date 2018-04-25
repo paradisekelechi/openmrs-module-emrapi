a880ca65990e0c09649e1f3a445bdc83853fe263
Paradise Kelechi <paradisekelechi@gmail.com>
2018-04-25 09:52:23 +0100

    TRUNK-5342: fix application context error modify serialization module version add quantifier annotation for the diagnosis service in the test add parent dependencies

--- api-2.2/src/test/java/org/openmrs/util/MigrateDiagnosisTest.java
+++ api-2.2/src/test/java/org/openmrs/util/MigrateDiagnosisTest.java
@@ -1,22 +1,16 @@
 package org.openmrs.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openmrs.Obs;
-import org.openmrs.Patient;
 import org.openmrs.api.ConceptService;
 import org.openmrs.api.PatientService;
 import org.openmrs.module.emrapi.EmrApiProperties;
-import org.openmrs.module.emrapi.diagnosis.Diagnosis;
 import org.openmrs.module.emrapi.diagnosis.DiagnosisMetadata;
+import org.openmrs.module.emrapi.diagnosis.DiagnosisService;
 import org.openmrs.module.emrapi.test.ContextSensitiveMetadataTestUtils;
-import org.openmrs.module.reporting.common.DateUtil;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -25,23 +19,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MigrateDiagnosisTest extends BaseModuleContextSensitiveTest {
 
+	private static  final String DIAGNOSIS_DATASET = "DiagnosisDataset.xml";
+
 	@Autowired
 	ConceptService conceptService;
 
 	@Autowired
-	EmrApiProperties emrApiProperties;
+	DiagnosisService emrapiDiagnosisService;
+
+	@Autowired
+	org.openmrs.api.DiagnosisService diagnosisService;
 
 	@Autowired
 	PatientService patientService;
 
 	@Autowired
-	org.openmrs.api.DiagnosisService newDiagnosisService;
-
-	DiagnosisMetadata dmd;
+	EmrApiProperties emrApiProperties;
 
 	@Before
 	public void setUp() throws Exception {
-		dmd = ContextSensitiveMetadataTestUtils.setupDiagnosisMetadata(conceptService, emrApiProperties);
+		executeDataSet(DIAGNOSIS_DATASET);
 	}
 
 	@Test
